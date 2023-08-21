549e721c3bf30b112ed2fad0b61ef1bb90d50f63
tenzin kunkyab <tenzinkunkyab5@gmail.com>
2023-08-21 19:16:39 -0400

    added html part

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("free tibet");
+
+const add = (a,b)=>{
+    return a+b;
+};
+
+const substract = (a,b)=>{
+    return a-b;
+}
+
+const multiply = (a,b)=>{
+    return a*b;
+}
+const divide = (a,b)=>{
+    return a/b;
+}
