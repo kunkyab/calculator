5755afc1be291b02ec0989792938a9d4a85326b0
tenzin kunkyab <tenzinkunkyab5@gmail.com>
2023-08-23 00:23:17 -0400

    added javascript code

--- index.js
+++ index.js
@@ -1,16 +1,122 @@
 console.log("free tibet");
 
-const add = (a,b)=>{
-    return a+b;
-};
 
-const substract = (a,b)=>{
-    return a-b;
+
+let operator = "";
+let previousValue = "";
+
+let currentValue= "";
+
+document.addEventListener("DOMContentLoaded", function() {
+
+let clear = document.querySelector(".clear");
+
+    let equal   = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previous = document.querySelector(".previous");
+    let current = document.querySelector(".current");
+numbers.forEach((number) => number.addEventListener ('click',function(e) {
+
+        handleNumber(e.target.textContent)
+        current.textContent = currentValue;
+        
+    }))
+
+    operators.forEach((op) => op.addEventListener("click", function(e) {
+        handleOperator(e.target.textContent)
+        previous.textContent = previousValue + " " + operator;
+        current.textContent = currentValue;
+
+    }))
+
+    clear.addEventListener("click", function () {
+        previousValue = "";
+        currentValue = "";
+        operator = "";
+        previous.textContent = currentValue;
+current.textContent = currentValue;
+    })
+
+
+    equal.addEventListener("click", function() {
+
+        if(currentValue !="" && previousValue != ""){
+
+      
+        calculate()
+        previous.textContent = "";
+         if (previousValue.length<=5) {
+            current.textContent = previousValue;
+         }
+
+        else{
+            current.textContent = previousValue.slice(0,5) +"...";
+        }
+    }
+    })
+
+
+    decimal.addEventListener("click", function() {
+       addDecimal();
+    })
+
+})
+
+
+
+
+function handleNumber(num) {
+    if (currentValue.length<=5) {
+        
+    
+    currentValue += num;
+}
 }
 
-const multiply = (a,b)=>{
-    return a*b;
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = "";
+  }
+  
+
+function calculate() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator ==="+"){
+        previousValue +=currentValue;
+
+    }
+    else if(operator==="-"){
+        previousValue -=currentValue;
+
+    }
+    else if(operator ==="*"){
+     previousValue *= currentValue;
+    }
+    else{
+        previousValue /=currentValue;
+    }
+
+     previousValue = round(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+
+
 }
-const divide = (a,b)=>{
-    return a/b;
+
+function round(num) {
+    return Math.round(num * 1000) /1000;
 }
+
+function addDecimal() {
+    if(!currentValue.includes(".")) {
+
+        currentValue +='.';
+    }
+}
+
